be37b10cb6eb2952caffde38cf4e94c8ad536b7a
Joshtdale <joshua.dale@student.awesomeinc.org>
2022-10-25 18:40:11 +0000

    more tests

--- src/main.py
+++ src/main.py
@@ -1,3 +1,14 @@
 class Character:
-    def __init__(self, name):
+    def __init__(self, name, alignment):
+        align = ['Good', 'Evil', 'Neutral']
         self.name = name
+        self.alignment = alignment
+
+
+
+
+
+
+p1 = Character('Josh', 'Evil')
+p2 = Character('Dakota', 'Good')
+print(p1.name, p2.name)
--- src/tests/test_iteration_1.py
+++ src/tests/test_iteration_1.py
@@ -1,9 +1,23 @@
 from main import *
 
 def test_is_character ():
-    p1 = Character('Josh')
+    # p1 = Character('Josh')
     assert Character is not None
 
 def test_is_name ():
-    p1 = Character('Josh')
+    # p1 = Character('Josh')
     assert p1.name is not None
+
+def test_name_unique ():
+    assert p1.name is not p2.name
+
+def test_alignment ():
+    assert p1.alignment is not None and p2.alignment is not None
+
+def test_p1_alignment_options ():
+    assert p1.alignment == 'Good' or p1.alignment == 'Evil' or p1.alignment == 'Neutral'
+
+def test_p2_alignment_options ():
+    assert p2.alignment == 'Good' or p2.alignment == 'Evil' or p2.alignment == 'Neutral'
+
+    
