4500fbefd830fa23812f24ac41160112f319ec18
Joshtdale <joshua.dale@student.awesomeinc.org>
2022-10-25 18:18:54 +0000

    we suck

--- src/main.py
+++ src/main.py
@@ -1,2 +1,3 @@
-class Character():
-    pass
+class Character:
+    def __init__(self, name):
+        self.name = name
--- src/tests/test_iteration_1.py
+++ src/tests/test_iteration_1.py
@@ -1,5 +1,9 @@
 from main import *
 
 def test_is_character ():
-    p1 = Character()
+    p1 = Character('Josh')
     assert Character is not None
+
+def test_is_name ():
+    p1 = Character('Josh')
+    assert p1.name is not None
