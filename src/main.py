623c53dfb2b35b0b2cf82028cfec19ec9f1af598
Joshtdale <joshua.dale@student.awesomeinc.org>
2022-10-25 19:30:22 +0000

    hp

--- src/main.py
+++ src/main.py
@@ -1,14 +1,12 @@
 class Character:
     def __init__(self, name, alignment):
-        align = ['Good', 'Evil', 'Neutral']
         self.name = name
         self.alignment = alignment
+        self.armor = 10
+        self.hp = 5
 
 
 
 
 
 
-p1 = Character('Josh', 'Evil')
-p2 = Character('Dakota', 'Good')
-print(p1.name, p2.name)
