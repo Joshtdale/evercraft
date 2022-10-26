ef24b7c5cc495e66dc8613d1b8c1deb7112d7d62
Joshtdale <joshua.dale@student.awesomeinc.org>
2022-10-26 17:40:43 +0000

    xp plus lots more

--- src/main.py
+++ src/main.py
@@ -1,36 +1,60 @@
+import math
 class Character:
+    modifiers = {
+        '1': -5,
+        '2': -4,
+        '3': -4,
+        '4': -3,
+        '5': -3,
+        '6': -2,
+        '7': -2,
+        '8': -1,
+        '9': -1,
+        '10': 0,
+        '11': 0,
+        '12': 1,
+        '13': 1,
+        '14': 2,
+        '15': 2,
+        '16': 3,
+        '17': 3,
+        '18': 4,
+        '19': 4,
+        '20': 5,
+    }
     def __init__(self, name, alignment):
-        modifiers = {
-            '14': 2,
-            '15': 3,
-            '16': 4,
-        }
         self.name = name
+        self.level = 1
+        self.xp = 0
         self.alignment = alignment
         self.armor = 10
         self.hp = 5
         self.alive = True
-        self.strength = 10 # modifiers[str(int('14') + int('1'))]
-        self.dexterity = 10
-        self.constitution = 10
-        self.wisdom = 10
-        self.intelligence = 10
-        self.charisma = 10
-
+        self.strength = self.modifiers['10'] # modifiers[str(int('14') + int('1'))]
+        self.dexterity = self.modifiers['10']
+        self.constitution = self.modifiers['10']
+        self.wisdom = self.modifiers['10']
+        self.intelligence = self.modifiers['10']
+        self.charisma = self.modifiers['10']
+        if self.xp < 1000:
+            self.level = 1
+        elif self.xp > 1000:
+            self.level = math.ceil(self.xp/1000)
 
     def attack(self, opponent, roll):
         if roll == 20:
             opponent.hp -= 2 + self.strength
-        elif roll >= 10:
+        elif roll >= opponent.armor:
             opponent.hp -= 1 + self.strength
         if opponent.hp <= 0:
             opponent.alive = False
 
 
+
 p1 = Character('Josh', 'Evil')
 p2 = Character('Dakota', 'Good')
-p1.attack(p2, 18)
-print (p2.strength)
+
+print (p1.level)
 
 
 
